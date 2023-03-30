cdfe2b937d4f3f59c53982e52e37e49d08869628
Dang Truong <truongminhdang26403@gmail.com>
2023-03-29 20:20:54 -0400

    Add automatic test script

---
+++ src/raft/test-raft.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+trap 'exit 1' INT
+
+export VERBOSE=0
+
+echo "Compiling raft.go..."
+go test -c -race
+chmod +x ./raft.test
+mkdir output
+echo "Running test $1 for $2 iterations..."
+for i in $(seq 1 "$2"); do
+    echo -ne "\r$i/$2 "
+    LOG="$1_$i.log"
+
+    if time ./raft.test -test.run "$1" -test.count=1 &> "output/$LOG"; then
+        echo "Success"
+        rm output/$LOG
+    else
+        echo "Failed - saving log at FAILED_$LOG"
+        mv "output/$LOG" "output/FAILED_$LOG"
+    fi
+done
+rm raft.test
